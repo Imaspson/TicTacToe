53f7ce61d7a084ebdfe8b02aece06637e6123c02
Imaspson <maier.samuel99@gmail.com>
2022-09-04 22:12:57 +0300

    Added the main classes for the game.

---
+++ Player.py
@@ -0,0 +1,27 @@
+import math
+import random
+
+#this is the base player class on top of which we 
+#will build a randomcomputerplayer and a humanplayer using
+#inheritance 
+
+class Player:
+    # letter is x or o
+    def __init__(self, letter):
+        self.letter = letter
+
+    # we want all players to get their next move
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        return super().get_move(game)
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
