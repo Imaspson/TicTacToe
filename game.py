e62621296a37371671f6796d4fb7ce939cf8cffa
Imaspson <maier.samuel99@gmail.com>
2022-09-05 12:02:50 +0300

    Added functions to keep track of the number of empty squares and if there are any left after a move.

--- game.py
+++ game.py
@@ -6,12 +6,29 @@ class TicTacToe:
         #keep track of the winner
 
     def print_board(self):
-        for row in [self.board[i * 3:(i + 1) * 3 for i in range(3)]]:
-            print('| ' + ' | ',join(row) + ' |')
+        for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
 
     @staticmethod
     def print_board_nums():
         # 0 | 1 | 2 | etc tells us that number corresponds to what box
         number_board = [[str(i) for i in range(j + 3, (j + 1) * 3)] for j in range(3)]
         for row in number_board:
-            print('| ' + ' | ',join(row) + ' |')
+            print('| ' + ' | '.join(row) + ' |')
+
+    def available_moves(self):
+        return [i for (i, spot) in enumerate(self.board) if spot == ' ']
+
+    def empty_squares(self):
+        return ' ' in self.board
+    
+    def num_empty_squares(self):
+        return len(self.available_moves())
+
+def play(game, x_player, o_player, print_game = True):
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X'
+
+    while game.empty_squares():
