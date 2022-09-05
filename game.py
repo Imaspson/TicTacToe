4eb56abbfbecd3c2fdc661360072afa3bc756cea
Imaspson <maier.samuel99@gmail.com>
2022-09-05 13:22:22 +0300

    Added functions to check for a win on rows columns and diagonals.

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+from Player import HumanPlayer, RandomComputerPlayer
+
+
 class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)]
@@ -33,6 +36,31 @@ class TicTacToe:
             return True
         return False
 
+    def winner(self, square, letter):
+        #check row
+        row_ind = square // 3 
+        row = self.board[row_ind * 3 : (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+
+        #check column
+        col_ind = square % 3 
+        column = [self.board[col_ind + i * 3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+
+        #check diagonals
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+
+        return False
+
+
 def play(game, x_player, o_player, print_game = True):
     if print_game:
         game.print_board_nums()
@@ -61,3 +89,13 @@ def play(game, x_player, o_player, print_game = True):
         letter = 'O' if letter == 'X' else 'X'
 
         #need a function to see if someone won
+
+        if print_game:
+            print("It's a tie!")
+
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game = True)
+
